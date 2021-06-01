7af18ca320da3f3b09718880f33478287b8aead7
Sanket Landage <sanket.landage@gmail.com>
2021-06-01 18:37:49 +0530

    Updated

--- Excercise5.java
+++ Excercise5.java
@@ -7,7 +7,7 @@ public class Excercise5 {
 	static void calculateSum(int n) {
 		int sum = 0;
 		for(int i=0 ; i<=n ; i++) {
-			if( ( i % 3 ) == 0 && ( i % 5 ) ==0) {
+			if( ( i % 3 ) == 0 || ( i % 5 ) ==0) {
 				sum = sum + i;
 			}
 		}

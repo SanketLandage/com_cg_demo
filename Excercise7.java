0b46ec5ea6f26c610d43416c5c1ef34cf61344b0
Sanket Landage <sanket.landage@gmail.com>
2021-06-01 18:46:30 +0530

    Excercise 7 done

--- Excercise7.java
+++ Excercise7.java
@@ -4,11 +4,24 @@ import java.util.Scanner;
 
 public class Excercise7 {
 	static void checkNumber(int n) {
-		int count=0;
-		for(int i=0 ; i<=n ; i++) {
+		int flag = 0;
+		int currentNumber = n % 10;
+		n = n/10;
+		while(n>0) {
+			if(currentNumber <= n%10) {
+				flag = 1;
+				break;
+			}
+			currentNumber = n %10;
+			n = n/10;
 			
 		}
-		System.out.println(count);
+		
+		if(flag == 1) {
+			System.out.println("False");
+		}else {
+			System.out.println("True");
+		}
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
